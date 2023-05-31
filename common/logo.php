8a9c02585c424d02e3393300fcca7ca69a287d57
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2023-06-01 01:04:56 +0300

    logo.php

---
+++ common/logo.php
@@ -0,0 +1,5 @@
+<a href="#" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-body-emphasis text-decoration-none "
+    aria-expanded="false">
+    <iconify-icon icon="heroicons:rocket-launch-solid" width="48" height="48" class="text-danger"></iconify-icon>
+    <p class="fs-2 text-danger mb-0"> Fusion</p>
+</a>
